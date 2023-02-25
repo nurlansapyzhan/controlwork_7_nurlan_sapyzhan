1d86c4bf5667fb20f6e21bcb2acf11158bf4ca2d
Nurlan Sapyzhan <nurlan_sapyzhan@mail.ru>
2023-02-25 12:34:38 +0600

    Admin, fixtures
    Добавил админку для модели, заполнил данными базу, сохранил данные в фикстуры

--- source/guest_book/admin.py
+++ source/guest_book/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from guest_book.models import Book
+
+
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('author', 'email', 'text', 'created_date', 'updated_date', 'status')
+    list_filter = ('author', 'email', 'status')
+    search_fields = ('author', 'email', 'status')
+    fields = ('author', 'email', 'text', 'created_date', 'updated_date', 'status')
+    readonly_fields = ('id', 'created_date', 'updated_date')
+
+
+admin.site.register(Book, BookAdmin)
--- source/guest_book/models.py
+++ source/guest_book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+STATUS_TYPES = (('active', 'Активно'), ('blocked', 'Заблокировано'))
+
+
+class Book(models.Model):
+    author = models.CharField(max_length=30, null=False, verbose_name='Имя автора')
+    email = models.EmailField(max_length=254, null=False, verbose_name='Почта автора записи')
+    text = models.TextField(null=False, verbose_name='Текст записи')
+    created_date = models.DateTimeField(auto_now=True, verbose_name='Время создания')
+    updated_date = models.DateTimeField(auto_now=False, verbose_name='Время редактирования', null=True)
+    status = models.CharField(max_length=50, null=False, choices=STATUS_TYPES, default=STATUS_TYPES[0][0], verbose_name='Статус')
